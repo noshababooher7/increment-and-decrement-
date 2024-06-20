ec4c0a018607f0a2915e3eb8bc590574f1918fac
Noshaba <noshababooher7@gmail.com>
2024-06-20 09:59:51 +0500

    Add files via upload

---
+++ increment.dart
@@ -0,0 +1,24 @@
+void main() {
+  // int number = 0;
+  // // int x = ++number; //pre  increment
+  // int y = number++; // post increment
+  // // print(x);
+  // print(y);
+
+  // int number1 = 1;
+  // // int x = number1--;
+  // int x = number1--;
+  // print(x);
+
+  var abc = 0;
+  var a = abc--;
+  var b = abc--;
+  var c = ++b;
+  var d = a++;
+  var f = --c;
+  var q = f++;
+  var w = q--;
+
+ var result = w++ + q--;
+  print(result);
+}
